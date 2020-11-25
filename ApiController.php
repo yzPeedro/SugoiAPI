363a203b7e91eb8f2d0882715a877f7cf0485075
yzPeedro <pedrocruzpessoa16@gmail.com>
2020-11-25 10:01:54 -0300

    remoção de 2 links

--- ApiController.php
+++ ApiController.php
@@ -20,9 +20,7 @@ class AnimesController
                 "https://cdn.superanimes.tv/010/animes/$anime_fc/$anime/$episode.mp4",//01
                 "https://ns569461.ip-51-79-82.net/$anime_FC/$anime/$episode.mp4",
                 "https://ns545982.ip-66-70-177.net/$anime_FC/$anime/$episode.mp4",
-                "https://cdn02.fluehost.com/a/$anime/hd/$episode.mp4", //01
-                "https://ns538468.ip-144-217-72.net/1/$anime/$episode.mp4", //01
-                "https://ns569568.ip-51-79-82.net/Uploads/Animes/$anime_FC/$anime/$episode.mp4" //01
+                "https://cdn02.fluehost.com/a/$anime/hd/$episode.mp4"
                 ];
                 foreach ( $links as $links_format ) {
                     if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
@@ -52,9 +50,7 @@ class AnimesController
                 "https://cdn.superanimes.tv/010/animes/$anime_fc/$anime/01.mp4",
                 "https://ns569461.ip-51-79-82.net/$anime_FC/$anime/01.mp4",
                 "https://ns545982.ip-66-70-177.net/$anime_FC/$anime/01.mp4",
-                "https://cdn02.fluehost.com/a/$anime/hd/02.mp4",
-                "https://ns538468.ip-144-217-72.net/1/$anime/01.mp4",
-                "https://ns569568.ip-51-79-82.net/Uploads/Animes/$anime_FC/$anime/01.mp4" 
+                "https://cdn02.fluehost.com/a/$anime/hd/02.mp4"
                 ];
                 foreach ( $links as $links_format ) {
                     if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require "ApiController.php";
 use Api\AnimesController;
 
 $app = new AnimesController();
-print_r($app->getEpisode('fairy tail', '1'));
+print_r($app->getEpisode('mirai nikki', '1'));
