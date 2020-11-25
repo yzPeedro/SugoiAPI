6130513650f6138271334356de4d03d947b4da6c
yzPeedro <pedrocruzpessoa16@gmail.com>
2020-11-25 14:05:50 -0300

    verifica se existe todos os episódios passados pelo parametro

--- ApiController.php
+++ ApiController.php
@@ -9,7 +9,7 @@ class AnimesController
         try {
             if ( $anime == '' || $episode == '' ) {
                 http_response_code(400);
-                return json_encode(["error" => "Bad request", "code" => "400"]);
+                return json_encode(["error" => "Bad request", "status" => "HTTP/1.1 400"]);
             } else {
                 $anime = str_replace([" ", "%20"], "-", strtolower($anime));
                 $anime_FC = ucfirst($anime[0]);
@@ -24,16 +24,16 @@ class AnimesController
                 foreach ( $links as $links_format ) {
                     if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
                         http_response_code(200);
-                        return json_encode(["anime" => $anime, "watch_in" => $links_format, "code" => "200"]);
+                        return json_encode(["anime" => $anime, "watch_in" => $links_format, "status" => "HTTP/1.1 200"]);
                     } else if ( $links_format == end($links) ) {
                         http_response_code(404);
-                        return json_encode(["error" => "Not Found", "code" => "404"]);
+                        return json_encode(["error" => "Not Found", "status" => "HTTP/1.1 404"]);
                     }
                 }
             }
         } catch (Exception $ex) {
             http_response_code(400);
-            return json_encode(["error" => "Bad request", "code" => "400"]);
+            return json_encode(["error" => "Bad request", "status" => "HTTP/1.1 400"]);
         }
     }
 
@@ -53,22 +53,85 @@ class AnimesController
                 foreach ( $links as $links_format ) {
                     if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
                         http_response_code(200);
-                        return json_encode(["anime" => $anime, "status_in_api" => "exists"]);
+                        return json_encode(["anime" => $anime, "status_in_api" => "exists", "status" => "HTTP/1.1 200"]);
                     } else if ( $links_format == end($links) ) {
                         http_response_code(404);
-                        return json_encode(["error" => "Not Found", "code" => "404"]);
+                        return json_encode(["error" => "Not Found", "status" => "HTTP/1.1 404"]);
                     }
                 }
             } else {
                 http_response_code(400);
-                return json_encode(["error" => "Bad request", "code" => "400"]);
+                return json_encode(["error" => "Bad request", "status" => "HTTP/1.1 400"]);
             }
         } catch (Exception $ex) {
             http_response_code(500);
-            return json_encode(["error" => "Internal Server Error", "code" => "500"]);
+            return json_encode(["error" => "Internal Server Error", "status" => "HTTP/1.1 500"]);
         }
     }
 
+
+
+    public function verifyIfExistsAllEpisodes( $anime = '', $episodes = '' )
+    {	
+    	try {
+    		if ( $anime == '' || $episodes == '' ) {
+    			http_response_code(400);
+    			return json_encode(["error" => "Bad Request", "status" => "HTTP/1.1 400"]);
+    		} else {
+    			$anime = str_replace([" ", "%20"], "-", strtolower($anime));
+                $anime_FC = ucfirst($anime[0]);
+                $anime_fc = $anime[0];
+                $links = [ 
+                "https://ns569461.ip-51-79-82.net/$anime_FC/$anime/01.mp4",
+                "https://ns545982.ip-66-70-177.net/$anime_FC/$anime/01.mp4",
+                "https://cdn.superanimes.tv/010/animes/$anime_fc/$anime/01.mp4",
+                "https://cdn02.fluehost.com/a/$anime/hd/01.mp4"
+                ];
+                foreach ( $links as $links_format ) {
+                    if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
+                        for( $i = "1"; ; $i++ ) {
+                        	if( $i < 10 ) {
+                        		$i = "0" . $i;
+                        	}
+                        	if ( $i >= 100 ) {
+                        		$links_format = str_replace(substr($links_format, -7), $i . substr($links_format, -4), $links_format);
+                        		if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
+                        			if ( $i == $episodes ) {
+										http_response_code(200);
+		                    			return json_encode(["anime" => $anime, "episodes_found" => $i, "found_all" => true, "link" => $links_format = str_replace(substr($links_format, -7), $i . substr($links_format, -4), $links_format)]);
+                        			} else {
+                        				continue;
+                        			}
+                        		} else {
+                        			http_response_code(200);
+	                    			return json_encode(["anime" => $anime, "episodes_found" => $i, "found_all" => false, "link" => $links_format = str_replace(substr($links_format, -7), $i . substr($links_format, -4), $links_format)]);
+                        		}
+                        	} else {
+                        		$links_format = str_replace(substr($links_format, -6), $i . substr($links_format, -4), $links_format);
+                        		if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
+                        			if ( $i == $episodes ) {
+										http_response_code(200);
+		                    			return json_encode(["anime" => $anime, "episodes_found" => $i, "found_all" => true, "link" => $links_format = str_replace(substr($links_format, -6), $i . substr($links_format, -4), $links_format)]);
+                        			} else {
+                        				continue;
+                        			}
+                        		} else {
+                        			http_response_code(200);
+	                    			return json_encode(["anime" => $anime, "episodes_found" => $i, "found_all" => false, "link" => $links_format = str_replace(substr($links_format, -6), $i . substr($links_format, -4), $links_format)]);
+                        		}
+                        	}
+                        }
+                    } else if ( $links_format == end($links) ) {
+                        http_response_code(404);
+                        return json_encode(["error" => "Not Found", "status" => "HTTP/1.1 404"]);
+                    }
+                }
+    		}
+    	} catch(Exception $ex) {
+			http_response_code(500);
+			return json_encode(["error" => "Internal Server Error", "status" => "HTTP/1.1 500"]);
+    	}
+    }
 
     public function countEpisodes( $anime = '')
     {
@@ -101,16 +164,16 @@ class AnimesController
 						}                  
                     } else if ( $links_format == end($links) ) {
                         http_response_code(404);
-                        return json_encode(["error" => "Not Found", "code" => "404"]);
+                        return json_encode(["error" => "Not Found", "status" => "HTTP/1.1 404"]);
                     }
                 }
     		} else {
     			http_response_code(400);
-    			return json_encode(["error" => "Bad Request", "code" => "400"]);	
+    			return json_encode(["error" => "Bad Request", "status" => "HTTP/1.1 400"]);	
     		}
     	} catch (Exception $ex) {
     		http_response_code(500);
-    		return json_encode(["error" => "Internal Server Error", "code" => "500"]);
+    		return json_encode(["error" => "Internal Server Error", "status" => "HTTP/1.1 500"]);
     	}
     }
 }
--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require "ApiController.php";
 use Api\AnimesController;
 
 $app = new AnimesController();
-print_r($app->countEpisodes('noragami'));
+print_r($app->verifyIfExistsAllEpisodes('noragami', '12'));
