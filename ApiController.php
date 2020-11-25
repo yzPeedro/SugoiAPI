815aa19e27669ed6fda6806d94a6424ed3aedb96
yzPeedro <pedrocruzpessoa16@gmail.com>
2020-11-25 12:57:51 -0300

    conta quantos episódios possui um anime

--- ApiController.php
+++ ApiController.php
@@ -14,7 +14,7 @@ class AnimesController
                 $anime = str_replace([" ", "%20"], "-", strtolower($anime));
                 $anime_FC = ucfirst($anime[0]);
                 $anime_fc = $anime[0];                
-                ($episode < 10 && substr($episode, -1)) ? $episode = "0" . $episode : false;
+                ($episode < 10 && substr($episode, 1) != "0") ? $episode = "0" . $episode : false;
                 $links = [ 
                 "https://ns569461.ip-51-79-82.net/$anime_FC/$anime/$episode.mp4",
                 "https://ns545982.ip-66-70-177.net/$anime_FC/$anime/$episode.mp4",
@@ -53,7 +53,7 @@ class AnimesController
                 foreach ( $links as $links_format ) {
                     if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
                         http_response_code(200);
-                        return json_encode(["anime" => $anime, "status in api" => "exists"]);
+                        return json_encode(["anime" => $anime, "status_in_api" => "exists"]);
                     } else if ( $links_format == end($links) ) {
                         http_response_code(404);
                         return json_encode(["error" => "Not Found", "code" => "404"]);
@@ -67,5 +67,50 @@ class AnimesController
             http_response_code(500);
             return json_encode(["error" => "Internal Server Error", "code" => "500"]);
         }
+    }
+
+
+    public function countEpisodes( $anime = '')
+    {
+    	try {
+    		if ( !empty($anime) ) {
+    			$anime = str_replace([" ", "%20"], "-", strtolower($anime));
+                $anime_FC = ucfirst($anime[0]);
+                $anime_fc = $anime[0];
+                $links = [ 
+                "https://ns569461.ip-51-79-82.net/$anime_FC/$anime/01.mp4",
+                "https://ns545982.ip-66-70-177.net/$anime_FC/$anime/01.mp4",
+                "https://cdn.superanimes.tv/010/animes/$anime_fc/$anime/01.mp4",
+                "https://cdn02.fluehost.com/a/$anime/hd/01.mp4"
+                ];
+                foreach ( $links as $links_format ) {
+                    if ( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
+						$count = '0';
+						while( true ) {
+							$count = $count + 1;
+							if ( $count < 10 ) {
+								$count = "0" . $count;
+							}
+							$links_format = str_replace(substr($links_format, -6), $count . substr($links_format, -4), $links_format);
+							if( get_headers($links_format)[2] == "Content-Type: video/mp4" || get_headers($links_format)[3] == "Content-Type: video/mp4" ) {
+								continue;
+							} else {
+								http_response_code(200);
+                        		return json_encode(["anime" => $anime, "episodes_found" => $count - 1, "link_last_episode_found" => $links_format = str_replace(substr($links_format, -6), $count - 1 . substr($links_format, -4), $links_format)]);
+							}
+						}                  
+                    } else if ( $links_format == end($links) ) {
+                        http_response_code(404);
+                        return json_encode(["error" => "Not Found", "code" => "404"]);
+                    }
+                }
+    		} else {
+    			http_response_code(400);
+    			return json_encode(["error" => "Bad Request", "code" => "400"]);	
+    		}
+    	} catch (Exception $ex) {
+    		http_response_code(500);
+    		return json_encode(["error" => "Internal Server Error", "code" => "500"]);
+    	}
     }
 }
